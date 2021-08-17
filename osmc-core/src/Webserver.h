a7e00d8d898e8fab4d11e5a255fd8eee6b378a82
Casper Thule <c.thulehansen@live.com>
2021-08-17 13:17:23 +0200

    Fixed test

---
+++ osmc-core/src/Webserver.h
@@ -0,0 +1,36 @@
+//
+// Created by Casper Thule on 16/08/2021.
+//
+
+#ifndef OSMCPROJECT_WEBSERVER_H
+#define OSMCPROJECT_WEBSERVER_H
+
+#include "httplib.h"
+#include <thread>
+const std::string outOfSyncPath = "/out-of-sync";
+
+class Webserver {
+
+public:
+    Webserver();
+    void startServer();
+    void RegisterOOSHandler(std::function<bool(void)> handler);
+    void startServer(int port);
+    bool isServerRunning();
+    ~Webserver();
+
+
+private:
+    httplib::Server svr;
+    const int defaultPort = 8080;
+    std::function<bool(void)> externalHandler;
+    std::thread _thd;
+    std::atomic<bool> started = std::atomic<bool>(false);
+
+    void stop();
+
+    std::function<void(const httplib::Request &, httplib::Response &)> oosGetHandler;
+};
+
+
+#endif //OSMCPROJECT_WEBSERVER_H
