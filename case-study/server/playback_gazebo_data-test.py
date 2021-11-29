9bb49f9a3612c8c5ac88671305012677484596c5
gitting-around <gitting-around@users.noreply.github.com>
2021-11-29 11:19:24 +0100

    updated playback script

--- case-study/server/playback_gazebo_data-test.py
+++ case-study/server/playback_gazebo_data-test.py
@@ -6,6 +6,7 @@ import time
 import csv
 import argparse
 import copy
+import math
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
@@ -21,6 +22,7 @@ time_sleep = 0.1 #should match the step size
 data = 'gazebo_playback_data-noround.csv'
 
 print(' [*] Waiting for logs. To exit press CTRL+C, sleep time [ms], matching step size: ', time_sleep*1000)
+EPSILON = 10 ** -12
 
 def publish(delay_mode, start_delay, end_delay, time_delay):
     msg_array = []
@@ -30,7 +32,7 @@ def publish(delay_mode, start_delay, end_delay, time_delay):
     msg['time']= dt.isoformat()
     msg['xpos']=0.0
     msg['ypos']=0.0
-    i = 1
+    i = 0.1
     with open(data, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -39,12 +41,12 @@ def publish(delay_mode, start_delay, end_delay, time_delay):
             ypos = float(row['ypos'])
             msg['xpos']=xpos
             msg['ypos']=ypos           
-            msg['seqno']=i
+            msg['seqno']=int(i*10)
             timet = datetime.datetime.strptime(t, "%Y-%m-%dT%H:%M:%S.%f%z")
             msg['time']= timet.isoformat()
 
             if delay_mode == "degrade":
-                if i >= start_delay and i< end_delay:
+                if i >= start_delay and i < end_delay:
                     print(" [DEGRADED] In degraded mode with delay %f" % time_delay)
                     time.sleep(time_delay)
                 else:
@@ -56,11 +58,12 @@ def publish(delay_mode, start_delay, end_delay, time_delay):
                 
             elif delay_mode == "drop":
                 period = end_delay-start_delay
-                if i >= start_delay and i < end_delay:
+                if i >= start_delay  and i < end_delay - EPSILON:
                     msg_array.extend([copy.deepcopy(msg)])
-                    time.sleep(1)
+                    time.sleep(time_sleep)
                     print(" [DROP] In drop mode with delay %f\n Messages will be sent in a burst after the delay period" % period)
-                elif i == end_delay:
+                    print(" [DROP] i %f" % i)
+                elif abs(i - end_delay) < EPSILON:
                     msg_array.extend([copy.deepcopy(msg)])
                     print(" [DROP END] After drop mode with delay %f\n Sending as fast as possible" % period)
                     #print(" [DROP END] Messages %s" % str(msg_array))
@@ -84,7 +87,7 @@ def publish(delay_mode, start_delay, end_delay, time_delay):
                             routing_key='default',
                             body=json.dumps(msg))
                 time.sleep(time_sleep)
-            i = i +1
+            i = i + 0.1
 
 def callback(ch, method, properties, body, delay_mode, start_delay, end_delay, time_delay):
     print(" [x] %r" % body)
@@ -102,8 +105,8 @@ if __name__ == '__main__':
 
     delay_mode = args.mode # options normal/degrade/drop
     print("Running in %s mode" % delay_mode)
-    start_delay = 5 # delay starts at this step
-    end_delay = 15 # delay ends at this step
+    start_delay = 2 # delay starts at this step
+    end_delay = 8 # delay ends at this step
     time_delay = 2 # delay between each publieshed message, bigger than the timestep, used as sleep between msg published in the degraded mode
 
     channel.basic_consume(
